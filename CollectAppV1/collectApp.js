63558994cba43cf430bb43b4181106b41ae53e6c
Charlie Grozier <charliegroz84@gmail.com>
2025-03-30 14:58:23 -0400

    space

--- CollectAppV1/collectApp.js
+++ CollectAppV1/collectApp.js
@@ -28,7 +28,7 @@ document.getElementById('movieForm').addEventListener('submit', function(event) 
                     detailsContainer.className = "details";
 
                     let detailsText = document.createElement('p');
-                    detailsText.innerHTML = "Overview:" + (movie.overview || "No description available.") + "<br>Release Date: " + (movie.release_date || "No release date available.") + "<br>Popularity: " + (movie.popularity || "No popularity available.");
+                    detailsText.innerHTML = "Overview: " + (movie.overview || "No description available.") + "<br>Release Date: " + (movie.release_date || "No release date available.") + "<br>Popularity: " + (movie.popularity || "No popularity available.");
                     detailsContainer.appendChild(detailsText);
 
                     let showDetailsButton = document.createElement('button');
