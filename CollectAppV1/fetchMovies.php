a562ff3eae36dfc8a1217245a3bb87968fce8fcb
Charlie Grozier <charliegroz84@gmail.com>
2025-03-30 12:38:00 -0400

    query

--- CollectAppV1/fetchMovies.php
+++ CollectAppV1/fetchMovies.php
@@ -3,20 +3,25 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 header('Content-Type: application/json');
 
-// Include the Composer autoloader
 require '../vendor/autoload.php';
 
-// Create a new Guzzle client
+$query = isset($_GET['query']) ? $_GET['query'] : '';
+
 $client = new \GuzzleHttp\Client();
 
-// Make the GET request to the API
-$response = $client->request('GET', 'https://api.themoviedb.org/3/search/movie?query=r&include_adult=false&language=en-US&page=10', [
+
+$response = $client->request('GET', 'https://api.themoviedb.org/3/search/movie', [
+    'query' => [
+        'query' => $query,
+        'include_adult' => false,
+        'language' => 'en-US',
+        'page' => 10,
+    ],
     'headers' => [
-        'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4M2E1MmQ3YzI4NTNiOWRlMDY2YWFiNDdlNWYzMDNjNSIsIm5iZiI6MTc0MzM0NTEzMy45NDksInN1YiI6IjY3ZTk1NWVkMmNjYTZmYzhmYmM2YmUxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u3olDlLJQMRyr9WKiQ-qqfsYkI_u759xVlyLHkyGt-w', 
-        'accept' => 'application/json',
+        'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4M2E1MmQ3YzI4NTNiOWRlMDY2YWFiNDdlNWYzMDNjNSIsIm5iZiI6MTc0MzM0NTEzMy45NDksInN1YiI6IjY3ZTk1NWVkMmNjYTZmYzhmYmM2YmUxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u3olDlLJQMRyr9WKiQ-qqfsYkI_u759xVlyLHkyGt-w',
+        'Accept' => 'application/json',
     ]
 ]);
 
-// Output the raw response body
 echo $response->getBody();
 ?>
