e36c63a12efef6e0f65ffad4fe6a02d1718dae4a
Charlie Grozier <charliegroz84@gmail.com>
2025-03-30 12:28:07 -0400

    error

--- CollectAppV1/fetchMovies.php
+++ CollectAppV1/fetchMovies.php
@@ -1,36 +1,22 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 header('Content-Type: application/json');
-require_once('vendor/autoload.php');
 
+// Include the Composer autoloader
+require '../vendor/autoload.php';
+
+// Create a new Guzzle client
 $client = new \GuzzleHttp\Client();
-/*
-$query = isset($_GET['query']) ? $_GET['query'] : '';
-$page = isset($_GET['page']) ? $_GET['page'] : 100;
 
-if(empty($query)){
-    echo json_encode(["error" => "No query provided"]);
-    exit;
-}
-
-$response = $client->request('GET', "https://api.themoviedb.org/3/search/movie", [
-    'headers' => [
-        'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4M2E1MmQ3YzI4NTNiOWRlMDY2YWFiNDdlNWYzMDNjNSIsIm5iZiI6MTc0MzM0NTEzMy45NDksInN1YiI6IjY3ZTk1NWVkMmNjYTZmYzhmYmM2YmUxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u3olDlLJQMRyr9WKiQ-qqfsYkI_u759xVlyLHkyGt-w',
-        'accept' => 'application/json',
-    ],
-    'query' => [
-        'query' => $query,
-        'include_adult' => 'false',
-        'language' => 'en-US',
-        'page' => $page,
-    ],
-]);
-*/
+// Make the GET request to the API
 $response = $client->request('GET', 'https://api.themoviedb.org/3/search/movie?query=reach&include_adult=false&language=en-US&page=10', [
     'headers' => [
-      'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4M2E1MmQ3YzI4NTNiOWRlMDY2YWFiNDdlNWYzMDNjNSIsIm5iZiI6MTc0MzM0NTEzMy45NDksInN1YiI6IjY3ZTk1NWVkMmNjYTZmYzhmYmM2YmUxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u3olDlLJQMRyr9WKiQ-qqfsYkI_u759xVlyLHkyGt-w',
-      'accept' => 'application/json',
-    ],
-  ]);
+        'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4M2E1MmQ3YzI4NTNiOWRlMDY2YWFiNDdlNWYzMDNjNSIsIm5iZiI6MTc0MzM0NTEzMy45NDksInN1YiI6IjY3ZTk1NWVkMmNjYTZmYzhmYmM2YmUxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u3olDlLJQMRyr9WKiQ-qqfsYkI_u759xVlyLHkyGt-w', 
+        'accept' => 'application/json',
+    ]
+]);
 
+// Output the raw response body
 echo $response->getBody();
 ?>
