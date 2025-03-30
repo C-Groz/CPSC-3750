68554cfabf01235abe55c287328124c1a6f618d3
Charlie Grozier <charliegroz84@gmail.com>
2025-03-30 12:34:55 -0400

    s

--- CollectAppV1/fetchMovies.php
+++ CollectAppV1/fetchMovies.php
@@ -10,7 +10,7 @@ require '../vendor/autoload.php';
 $client = new \GuzzleHttp\Client();
 
 // Make the GET request to the API
-$response = $client->request('GET', 'https://api.themoviedb.org/3/search/movie?query=reach&include_adult=false&language=en-US&page=10', [
+$response = $client->request('GET', 'https://api.themoviedb.org/3/search/movie?query=r&include_adult=false&language=en-US&page=10', [
     'headers' => [
         'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4M2E1MmQ3YzI4NTNiOWRlMDY2YWFiNDdlNWYzMDNjNSIsIm5iZiI6MTc0MzM0NTEzMy45NDksInN1YiI6IjY3ZTk1NWVkMmNjYTZmYzhmYmM2YmUxNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.u3olDlLJQMRyr9WKiQ-qqfsYkI_u759xVlyLHkyGt-w', 
         'accept' => 'application/json',
