c4ee68939ca8bea7fe03e469a2241ff8eaa6c453
Charlie Grozier <charliegroz84@gmail.com>
2025-04-03 10:39:06 -0400

    db test

---
+++ Database/config.php
@@ -0,0 +1,8 @@
+<?php
+$dbConfig = array(
+    'host'     => 'localhost',        
+    'username' => 'u461793670_Charlie', 
+    'passwd'   => 'DatabasePW123',
+    'dbname'   => 'u461793670_database'    
+);
+?>
--- Database/database.php
+++ Database/database.php
@@ -1,27 +1,28 @@
 <?php
-
-//require_once 'config.php';
+require_once 'config.php'; 
 
 $link = mysqli_connect(
-    $dbConfig['localhost'],
-    $dbConfig['u461793670_Charlie'],
-    $dbConfig['DatabasePW123'],
-    $dbConfig['u461793670_database']
+    $dbConfig['host'],
+    $dbConfig['username'],
+    $dbConfig['passwd'],
+    $dbConfig['dbname']
 );
 
 if (!$link) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+// Handle form submission
 if (isset($_POST['person'])) {
-    // filter input
     $person = mysqli_real_escape_string($link, $_POST['person']);
     $sql = "INSERT INTO person (name) VALUES ('" . $person . "')";
     mysqli_query($link, $sql);
 }
 
+// Fetch people from the database
 $sql = 'SELECT person_id, name FROM person';
-$people = array ();
+$people = array();
+
 if (mysqli_real_query($link, $sql)) {
     if ($result = mysqli_store_result($link)) {
         while ($row = mysqli_fetch_assoc($result)) {
@@ -33,24 +34,28 @@ if (mysqli_real_query($link, $sql)) {
 mysqli_close($link);
 ?>
 <!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
     <title>People</title>
 </head>
-<script>
-    fetch("../navbar.html")
-      .then(response => response.text())
-      .then(data => document.body.insertAdjacentHTML("afterbegin", data));
-</script>
 <body>
+    <script>
+        fetch("../navbar.html")
+            .then(response => response.text())
+            .then(data => document.body.insertAdjacentHTML("afterbegin", data));
+    </script>
+
     <ul>
-        <? foreach ($people as $id => $name): ?>
+        <?php foreach ($people as $id => $name): ?>
             <li><a href="<?= $_SERVER['PHP_SELF'] ?>?id=<?= urlencode($id) ?>"><?= htmlspecialchars($name) ?></a></li>
-        <? endforeach; ?>
+        <?php endforeach; ?>
     </ul>
+
     <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
         <dl>
             <dt>Add person</dt>
-            <dd><input type="text" name="person" /></dd>
+            <dd><input type="text" name="person" required /></dd>
         </dl>
         <input type="submit" name="submit" value="Submit" />
     </form>
