d02f290e26eede97e168680f477474ff2a51443e
Charlie Grozier <charliegroz84@gmail.com>
2025-04-26 15:18:51 -0400

    s

--- FinalProject/calendar_functions.js
+++ FinalProject/calendar_functions.js
@@ -4,13 +4,13 @@
     $('#datePicker').append('<select id="month"></select>');
   
     for(var i = 0; i < months.length;i++) {
-       $('#month').append('<option value="'+i+'">'+months[i]+'</option')
+      $('#month').append('<option value="'+i+'">'+months[i]+'</option')
     }
 
     $('#datePicker').append('<select id="year"></select>');
 
     for(i = 1990; i < 2021; i++) {
-       $('#year').append('<option value="'+i+'">'+i+'</option>')
+      $('#year').append('<option value="'+i+'">'+i+'</option>')
     }
 
     $('#datePicker').append('<button id="submit">Go!</button>');
@@ -27,47 +27,47 @@
      $("#myCal").empty();
 
     if (date == null) {
-       date = new Date;
-     }
+      date = new Date;
+    }
 
-     day = date.getDate();
-     month = date.getMonth();
-     year = date.getFullYear();
-  
-     months = new Array('January','February','March','April','May','June', 'July','August','September','October','November','December');
-     this_month = new Date(year, month, 1);
-     next_month = new Date(year, month + 1, 1);
-   
-     days = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
-     first_week_day = this_month.getDay(); // day of the week of the first day
-     days_in_this_month = Math.round((next_month.getTime() - this_month.getTime())  / (1000 * 60 * 60 * 24));
+    day = date.getDate();
+    month = date.getMonth();
+    year = date.getFullYear();
 
-     $('#myCal').append('<table id="myCalendar"></table>');
-     $('#myCalendar').append('<thead><tr></tr></thead>');
+    months = new Array('January','February','March','April','May','June', 'July','August','September','October','November','December');
+    this_month = new Date(year, month, 1);
+    next_month = new Date(year, month + 1, 1);
   
-     for (var i=0; i < days.length; i++) {
-       $('#myCalendar thead tr').append('<th>'+days[i]+'</th>')
-     }
-  
-     $('#myCalendar').append('<tbody></tbody>');
-     $('tbody').append('<tr>');
-   
-     for(week_day = 0; week_day < first_week_day; week_day++)  {
-       $('tbody tr').append('<td id="'+week_day+'"></td>');
-     }
-  
-     week_day = first_week_day;
+    days = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
+    first_week_day = this_month.getDay(); 
+    days_in_this_month = Math.round((next_month.getTime() - this_month.getTime())  / (1000 * 60 * 60 * 24));
 
-     for (day_counter=1; day_counter <= days_in_this_month; day_counter++) {
-       week_day %= 7;
-     
-       if (week_day == 0) {
-         // go to the next line of the calendar
-         $('tbody').append('</tr><tr>');
-       }
+    $('#myCal').append('<table id="myCalendar"></table>');
+    $('#myCalendar').append('<thead><tr></tr></thead>');
 
-       $('tbody tr:last').append('<td id="day'+day_counter+'">' + day_counter + '</td>');
+    for (var i=0; i < days.length; i++) {
+      $('#myCalendar thead tr').append('<th>'+days[i]+'</th>')
+    }
+
+    $('#myCalendar').append('<tbody></tbody>');
+    $('tbody').append('<tr>');
   
-       week_day++;
-     }
-  }
+    for(week_day = 0; week_day < first_week_day; week_day++)  {
+      $('tbody tr').append('<td id="'+week_day+'"></td>');
+    }
+
+    week_day = first_week_day;
+
+    for (day_counter=1; day_counter <= days_in_this_month; day_counter++) {
+      week_day %= 7;
+    
+      if (week_day == 0) {
+        // go to the next line of the calendar
+        $('tbody').append('</tr><tr>');
+      }
+
+      $('tbody tr:last').append('<td id="day'+day_counter+'">' + day_counter + '</td>');
+
+      week_day++;
+    }
+}
